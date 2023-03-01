793294745fd2725a568e1bbf71084f8468f7a931
Ved Dadhich <dadhichved1997@gmail.com>
2023-03-01 22:12:58 +0530

    created calculator using html css and javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+let scrn = document.getElementById("screen");
+function input(x){
+    scrn.value = scrn.value+x;
+}
+
+function allClear(){
+    scrn.value='';
+}
+
+function del(){
+    scrn.value=scrn.value.slice(0,-1);
+}
+
+function equal(){
+    try {
+        scrn.value = eval(scrn.value);
+    } catch (error) {
+      scrn.value = 'Error';
+    }
+}
